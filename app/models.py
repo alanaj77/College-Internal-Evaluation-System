bc3e80faf3b0b87d7af7e75eab99d6878f916bf5
Alan Joseph <alanjoseph52006@gmail.com>
2026-04-10 00:37:47 +0530

    added a requirement.txt

--- app/models.py
+++ app/models.py
@@ -2,28 +2,20 @@ import mysql.connector
 from mysql.connector import Error
 import os
 
-
 def get_connection():
     try:
-
-        # This ensures the code finds ca.pem regardless of where you run the script from
-        base_path = os.path.dirname(os.path.abspath(__file__))
-        cert_path = os.path.join(base_path, "ca.pem")
-
-        conn =  mysql.connector.connect(
-            host="mysql-2420b97f-alanjoseph.j.aivencloud.com",
-            port=19150,
-            user="avnadmin",
-            password="AVNS_-omvYPudqpObIUs4hqd",
-            database="collegedb",
-            
-        
-            use_pure=True  # <--- CRITICAL: This fixes the Windows SSL error
+        conn = mysql.connector.connect(
+            host=os.environ.get("DB_HOST"),
+            port=int(os.environ.get("DB_PORT", 3306)),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME"),
+            use_pure=True
         )
+
         if conn.is_connected():
             return conn
+
     except Error as e:
         print(f"Error while connecting to Mysql: {e}")
-        return None 
-
-    
+        return None
